bf39a76fc30f4691a7243e0276d3c5aab4e5e7c5
jblanquicett92 <jorgeabm1992@gmail.com>
2022-12-13 01:29:10 +0000

    feat(s1): s1

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
 /* function config(req, res, next) {
   res.setHeader('Content-Type', 'application/json');
   next()
 }
 app.use(config) */
-
 app.use(express.json());
 
 app.get('/', (req, res) => {
