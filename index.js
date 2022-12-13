88be00ddb97ed4fc66fa349ae1420534d90a8d3d
jblanquicett92 <jorgeabm1992@gmail.com>
2022-12-13 01:22:26 +0000

    s1

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+/* function config(req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  next()
+}
+app.use(config) */
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send(JSON.stringify({ status: "Running on express" }))
 })
